2a408fc75192772eab9b5c0baf7a99883a32948d
tavianovitaliari <tavianovitaliari@gmail.com>
2024-10-31 16:40:33 +0300

    lmrDepth
    bench: 1323642

--- src/search.cpp
+++ src/search.cpp
@@ -1045,7 +1045,7 @@ moves_loop:  // When in check, search starts here
                     continue;
                 }
 
-                lmrDepth = std::max(lmrDepth, 0);
+                lmrDepth = std::max(lmrDepth, int(!allNode));
 
                 // Prune moves with negative SEE (~4 Elo)
                 if (!pos.see_ge(move, -24 * lmrDepth * lmrDepth))
